c70a5290f8ebe8a8bdf26eb95c3ee2c765f90d87
Landon Smith <creeperthemighty@gmail.com>
2026-02-09 13:47:34 -0700

    arrays finished

---
+++ ponder/arrays.js
@@ -0,0 +1,86 @@
+let pets = ['goldfish', 'dog', 'rhino'];
+
+console.log(pets.length);
+
+pets[0] = 'bunny';
+
+pets.push('lizard');
+
+console.log(pets);
+
+const steps = ['one', 'two', 'three'];
+
+//.forEach call a function for every item in the array
+steps.forEach(function(item) {
+    console.log(item);
+});
+
+steps.forEach(showStep);
+function showStep(item) {
+    console.log(item);
+}
+
+let myList = document.querySelector('#myList');
+
+//.map also calls a function but creates a new array from the original array
+
+const stepsHtml = steps.map(listTemplate);
+
+function listTemplate(item) {
+    return `<li>${item}</li>`;
+}
+
+myList.innerHTML = stepsHtml.join('');
+
+//.map
+
+let grades = ['A', 'B', 'D'];
+
+let points; 
+
+let gpaPoints = grades.map(convert)
+
+function convert(grade) {
+    switch (grade) {
+        case 'A':
+            points = 4;
+            break;
+        case 'B':
+            points = 3;
+            break;
+        case 'C':
+            points = 2;
+            break;
+        case 'D':
+            points = 1;
+            break;
+        case 'F':
+            points = 0;
+            break;
+        default:
+            alert('Invalid grade: ' + grade);
+    }
+    return points;
+}
+
+console.log(gpaPoints);
+console.log(grades);
+
+//.reduce -- reduce down to a single value with an accumulator
+
+let totalPoints = gpaPoints.reduce(getTotal);
+
+function getTotal(total, item) {
+    return total + item;
+}
+
+console.log(totalPoints);
+let gpaAverage = totalPoints / gpaPoints.length;
+console.log(gpaAverage);
+
+//.filter -- filter out items that do not meet a condition
+
+const words = ['watermelon', 'peach', 'apple', 'tomato', 'grape'];
+const shortWords = words.filter(function(word){
+    return word.length <= 6;
+});
